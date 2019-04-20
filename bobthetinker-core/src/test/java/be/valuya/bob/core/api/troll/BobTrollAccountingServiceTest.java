0e9042a6ad6059abf289f8c77573319b4376b68a
cghislai <charlyghislain@gmail.com>
2019-04-20 14:28:21 +0300

    test category

--- bobthetinker-core/src/test/java/be/valuya/bob/core/api/troll/BobTrollAccountingServiceTest.java
+++ bobthetinker-core/src/test/java/be/valuya/bob/core/api/troll/BobTrollAccountingServiceTest.java
@@ -1,15 +1,19 @@
 package be.valuya.bob.core.api.troll;
 
 import be.valuya.bob.core.BobFileConfiguration;
+import be.valuya.bob.core.test.categories.LocalDev;
 import be.valuya.bob.core.util.print.BobThePrinter;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+@Category(LocalDev.class)
 @RunWith(JUnit4.class)
 public class BobTrollAccountingServiceTest {
 
---
+++ bobthetinker-core/src/test/java/be/valuya/bob/core/test/categories/LocalDev.java
@@ -0,0 +1,4 @@
+package be.valuya.bob.core.test.categories;
+
+public class LocalDev {
+}
